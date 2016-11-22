1846215782b265a82af0fe42789155803108bf1a
Juan José Cacho <juanjo@aumentha.com>
2016-11-22 18:34:40 +0100

    .

--- src/entries/chunks/localization.ts
+++
@@ -1,6 +0,0 @@
-import '../../localization/russian';
-import '../../localization/french';
-import '../../localization/finnish';
-import '../../localization/german';
-import '../../localization/dutch';
-import '../../localization/turkish';
